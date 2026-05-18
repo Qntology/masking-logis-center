1c73fab1577f13a9fb044da9f272f801645a7ec3
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-05-18 15:16:18 +0900

    oo

--- run_cargo.bat
+++ run_cargo.bat
@@ -1,7 +1,9 @@
 @echo off
 chcp 65001 > nul
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+chcp 65001 > nul
 set "PYTHONIOENCODING=utf-8"
+set "LC_ALL=C.UTF-8"
 
 :: [AMD ROCm/HIP]
 set "CANDLE_HIP=1"
@@ -14,4 +16,5 @@ rem Add DirectStorage DLLs to PATH
 set "PATH=%PATH%;%CD%\microsoft.direct3d.directstorage.1.3.0\native\bin\x64"
 
 echo [RUN] Compiling and starting application (UTF-8 Mode)...
-cargo run --features cuda
+cargo run --features cuda --features cuda > ..\run_result.txt 2>&1
+type ..\run_result.txt
