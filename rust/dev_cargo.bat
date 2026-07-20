2905a187522562bc65639a01f431d981fe167db7
SungU Che <127634415+sungu-che@users.noreply.github.com>
2026-07-21 03:42:10 +0900

    oo

--- rust/dev_cargo.bat
+++ rust/dev_cargo.bat
@@ -1,31 +1,17 @@
 @echo off
 chcp 65001 > nul
 call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-set "MSVC_BIN=C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Tools\MSVC\14.51.36231\bin\Hostx64\x64"
+set "NVCC_CCBIN=C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Tools\MSVC\14.51.36231\bin\Hostx64\x64\cl.exe"
 set "PYTHONIOENCODING=utf-8"
 
-:: 실행 시 넘겨준 인자(cuda 또는 rocm)를 받습니다. 기본값은 cuda입니다.
-set TARGET_GPU=%1
-if "%TARGET_GPU%"=="" set TARGET_GPU=cuda
-
-if "%TARGET_GPU%"=="cuda" (
-    echo [ENV] Setting up CUDA Environment...
-    set "NVCC_CCBIN=%MSVC_BIN%\cl.exe"
-    set "NVCC_PREPEND_FLAGS=-Xcompiler /Zc:preprocessor"
-    set "CUDA_PATH=%CD%\src-tauri\dlls\cuda"
-    set "CUDA_ROOT=%CD%\src-tauri\dlls\cuda"
-    set "CUDA_TOOLKIT_ROOT_DIR=%CD%\src-tauri\dlls\cuda"
-    set "PATH=%MSVC_BIN%;%CD%\src-tauri\dist_cuda;%CD%\src-tauri\dlls\cuda\bin;%CD%\src-tauri\dlls\cuda\lib;%CD%\src-tauri\dlls\cuda\include;%PATH%"
-)
-
-if "%TARGET_GPU%"=="rocm" (
-    echo [ENV] Setting up AMD ROCm Environment...
-    set "CANDLE_HIP=1"
-    set "HIP_PATH=C:\Program Files\AMD\ROCm\7.1"
-    set "PATH=%MSVC_BIN%;%CD%\src-tauri\dist_rocm;%HIP_PATH%\bin;%PATH%"
-)
+:: 2. CUDA 13.x 용 표준 전처리 가속 플래그 설정 (CCCL 빌드 에러 방지)
+set "NVCC_PREPEND_FLAGS=-Xcompiler /Zc:preprocessor"
 
 cd src-tauri
 
-echo [DEV] Starting Tauri application for %TARGET_GPU%...
-cargo tauri dev -- --features %TARGET_GPU%
+set "PATH=%CD%\dlls;%PATH%"
+
+echo [DEV] Starting Tauri application (Development Mode)...
+
+:: build 대신 dev 명령어를 사용하여 디버깅 모드로 앱을 실행합니다.
+cargo tauri dev -- --features cuda
